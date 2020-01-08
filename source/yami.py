92deb8fd012d072da961d305f60a3f1ae1db65bf
BoxBoy (Hong Joongi) <joongi1978@naver.com>
2020-01-09 00:46:09 +0900

    1.3 엑셀 파일로 저장 구현
    원하는 달 등을 입력하면 엑셀 파일로 저장하도록 구현

--- source/yami.py
+++ source/yami.py
@@ -1,8 +1,8 @@
 # 임포트 부분
 import requests
 import json
-import re
 from bs4 import BeautifulSoup
+import openpyxl
 
 school_infos = 0  # school_code 함수 사용을 위한 전역변수 선언
 hang = 0  # 급식 정보가 있는 행 정보 저장을 위한 전역변수 선언
@@ -42,16 +42,16 @@ def school_code(num):
     return(school_code)  # 반환
 
 
-# ~~ 학교 급식을 불러오는 파트 ~~
+# ~~ 학교 급식을 불러오고 저장하는 파트 ~~
 
-# 3. 학교 급식 불러오기 (주간)
+# 3. 학교 급식 불러오기
 
-def school_food(ooe, code, ymd, sclass, food):
+def school_food(ooe, code, ymd, sclass, kindfood):
     url = "http://stu." + ooe + ".go.kr/sts_sci_md01_001.do?" +\
         "schulCode=" + code +\
         "&schulCrseScCode=" + sclass +\
         "&schulKnaScCode=0" + sclass +\
-        "&schMmealScCode=" + food +\
+        "&schMmealScCode=" + kindfood +\
         "&schYmd=" + ymd
     # ooe는 교육청, code는 학교고유코드, sclass는 교급, year와 month는 조회 년월
 
@@ -80,11 +80,11 @@ def school_food(ooe, code, ymd, sclass, food):
         for sakje in date_filter2:
             date2 = date2.replace(sakje, '')
 
-        if date2 != ymd:
+        if date2 != ymd:  # 날짜와 입력날짜가 다를 경우 패스
             continue
 
         hang = i - 1  # 급식정보가 있는 행 선언
-        origin_ymd = date  # yyyy.mm.dd (요일) 형식 날짜 선언
+        # origin_ymd = date  # yyyy.mm.dd (요일) 형식 날짜 선언
         break
 
     # 급식 정보 가져오기
@@ -98,14 +98,49 @@ def school_food(ooe, code, ymd, sclass, food):
     for sakje in food_filter:
         food = food.replace(sakje, '')  # 찌끄레기 삭제
 
-    food = food.replace('<br/>', '\n')  # html 코드 변환
+    food = food.replace('<br/>', '`n')  # html 코드 변환
 
-    # 깔끔하게 반환
-    food = origin_ymd + " 급식 정보\n\n" +\
-        food +\
-        "\n본 정보는 학교 사정에 따라 수정될 수 있습니다."
+    if food == ' ':
+        food = '급식이 예정되지 않았거나 급식 정보가 없습니다.\n'
 
-    return(food)
+    output = [date, food]
+
+    return(output)
 
 
-print(school_food('goe', 'J100005611', '2019.09.16', '3', '2'))
+# 4. 학교 급식 저장하기 (월간)
+
+def school_food_save(ooe, code, year, month, sclass, kindfood):
+    xx = openpyxl.Workbook()
+
+    sheet = xx.active
+    sheet.title = 'food'
+    sheet['A1'] = '날짜'
+    sheet['B1'] = '급식 정보'
+
+    # 날짜 구하기
+    if month == '02':  # 2월 조회시
+        if year % 4 == 0 and year % 100 != 0:  # 윤년일 경우 29일이 마지막
+            last_day = 29
+        elif year % 400 == 0:
+            last_day = 29
+        else:  # 아니면 28이 마지막
+            last_day = 28
+    elif month == '01' or month == '03' or month == '05' or month == '07' or month == '08' or month == '10' or month == '12':  # 끝날이 31일 목록
+        last_day = 31
+    else:
+        last_day = 30
+
+    for day in range(1, last_day + 1):  # 하루 씩 day에 대입
+        xday = day  # 엑셀용 날짜 저장
+        if day < 10:
+            day = '0' + str(day)
+        meal = school_food(ooe, code, year + '.' +
+                           month + '.' + str(day), sclass, kindfood)  # 급식정보 불러옴
+        sheet.cell(row=xday + 1, column=1).value = meal[0]  # 엑셀에 저장
+        sheet.cell(row=xday + 1, column=2).value = meal[1]
+
+    xx.save('Yami_' + year + '_' + month + ".xlsx")  # 엑셀 파일 생성
+
+
+school_food_save('goe', 'J100005611', '2019', '10', '3', '2')
